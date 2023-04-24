d44b9f3cfba2e495bc2bd70238075f8a4be3856d
Ashkan Faghihi <ashkan117@yahoo.com>
2023-04-23 20:33:02 -0700

    Incomplete but cleaner version, will remove appending to list to save on memory

--- main.py
+++ main.py
@@ -1,68 +1,34 @@
-from time import sleep
 from typing import Tuple, List
 import decimal
 import typer
 import requests
 from datetime import datetime, timedelta
 
-
-# decimal.getcontext().rounding = decimal.ROUND_CEILING
-# decimal.getcontext().rounding = decimal.ROUND_DOWN
-# decimal.getcontext().rounding = decimal.ROUND_FLOOR
-# decimal.getcontext().rounding = decimal.ROUND_HALF_DOWN
-# decimal.getcontext().rounding = decimal.ROUND_HALF_EVEN
-# decimal.getcontext().rounding = decimal.ROUND_HALF_UP
-# decimal.getcontext().rounding = decimal.ROUND_UP
-# decimal.getcontext().rounding = decimal.ROUND_05UP
-# decimal.getcontext().prec = 7
-time_format = "%Y-%m-%dT%H:%M:%SZ"
+TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 SECONDS_IN_DAY = 86400
 
 def convert_datetime_to_string(dt: datetime):
-    return datetime.strftime(dt, time_format)
+    return datetime.strftime(dt, TIME_FORMAT)
 
 def main(
-    start: datetime = typer.Argument( ..., formats=[time_format]),
-    end: datetime = typer.Argument( ..., formats=[time_format]),
+    start: datetime = typer.Argument( ..., formats=[TIME_FORMAT]),
+    end: datetime = typer.Argument( ..., formats=[TIME_FORMAT]),
 ):
-    # Accepts two timestamps (in RFC3339 format) as command-line arguments. 
-    # Each timestamp represents a particular hour. That is, the "minute" and "second" fields should be zero. 
-    # For example, 2021-05-12T19:00:00Z.
-
-    # response = requests.get('https://tsserv.tinkermode.dev/data?begin=2021-03-04T03:00:00Z&end=2021-03-04T11:59:59Z')
-    
-    # response = requests.get(f'https://tsserv.tinkermode.dev/data?begin={convert_datetime_to_string(start)}&end={convert_datetime_to_string(end)}')
-    '''
-    We will split up the times into 14 hour chunks. The reasoning behind this is that there are 50K seconds in 14 hours. So to make sure that there aren't 
-    too many elements being processed.
-    '''
     current_day: datetime = start
     total_difference = end - current_day
     items: List[Tuple[str, float]]  = []
-    print(start, end, total_difference)
     while total_difference.total_seconds() > SECONDS_IN_DAY:
-        print("Starting with ")
         current_day_end = datetime(current_day.year, current_day.month, current_day.day, 23,59,59)
-        print("Current day, end, total_difference")
-        print(current_day, current_day_end, current_day_end - current_day)
         response = requests.get(f'https://tsserv.tinkermode.dev/data?begin={convert_datetime_to_string(current_day)}&end={convert_datetime_to_string(current_day_end)}')
         output = process_data(response)
         items.extend(output)
         diff = current_day_end - current_day + timedelta(seconds=1)
         current_day += diff
-        print(f'next day would be {current_day}')
-        # diff = next_day - current_day_end 
-        # current_day = current_day + timedelta(0,diff.total_seconds())
         total_difference = end - current_day
-        sleep(1)
 
     if total_difference.total_seconds() >= 0:
-        print("The total difference is now one day but not zero")
-        print("Current day, end, total_difference")
-        print(current_day, end, total_difference)
         response = requests.get(f'https://tsserv.tinkermode.dev/data?begin={convert_datetime_to_string(current_day)}&end={convert_datetime_to_string(end)}')
         output = process_data(response)
-        print(output)
         items.extend(output)
     response = requests.get(f'https://tsserv.tinkermode.dev/data?begin={convert_datetime_to_string(start)}&end={convert_datetime_to_string(end)}')
     output = process_data(response)
@@ -87,20 +53,26 @@ def process_data(response: requests.Response) -> List[Tuple[str, float]]:
     for hour, tup in hourly_buckets.items():
         total, count = tup
         hour = hour + ":00:00Z"
-        help = decimal.Decimal(total / count)
-        # print(help)
-        results.append((hour, float(round(help, 4))))
-        print(f'rounding {total/count} resulted in {round(total / count, 4)} and the decimal version is {round(help, 4)}')
+        average = decimal.Decimal(total / count)
+        rounded_average = float(round(average, 4))
+        print(f'{hour} {rounded_average}')
+        results.append((hour, rounded_average))
     
-    print(f'Processed {len(results)} items')
     return results
 
 def digest_request(text: str) -> List[Tuple[str, float]]:
+    '''
+    We are reading the request from the data endpoint and transforming it into a tuple
+    '''
+    # last element is a new line character
     time_values = text.split('\n')[:-1]
     results : List[Tuple[str, float]] = []
     for time_value in time_values:
+        # every row looks like the one below
         # 2021-03-04 03:45:14 110.8634
         words = time_value.split(' ')
+        # there are some cases where there is an additional space in the words array
+        # [time, ' ', value]
         if len(words) > 2:
             time, value = words[0], words[2]
         else: 
@@ -111,4 +83,3 @@ def digest_request(text: str) -> List[Tuple[str, float]]:
 
 if __name__ == "__main__":
     typer.run(main)
-    # 2021-03-04T03:00:00Z 2021-03-04T11:59:59Z')
--- test_main.py
+++ test_main.py
@@ -1,102 +1,49 @@
 from datetime import datetime
-time_format = "%Y-%m-%dT%H:%M:%SZ"
 from main import convert_datetime_to_string, main, digest_request
 import requests
 
-def runner(start, end, expected):
-    start_datetime = datetime.strptime(start,time_format)
-    end_datetime = datetime.strptime(end,time_format)
+def test_one():
+    start = datetime(2021, 3, 4, 4)
+    end = datetime(2021, 3, 4, 11, 59, 59)
 
-    assert main(start_datetime, end_datetime) == expected
+    actual = main(start, end)
+    output_response = requests.get(f'https://tsserv.tinkermode.dev/hourly?begin={convert_datetime_to_string(start)}&end={convert_datetime_to_string(end)}')
+    output = digest_request(output_response.text)
+    assert actual == output
 
-# def test_one():
-#     start = datetime(2021, 3, 4, 4)
-#     end = datetime(2021, 3, 4, 11, 59, 59)
+def test_two():
+    start = datetime(2021, 3, 2, 4)
+    end = datetime(2021, 3, 8, 11, 59, 59)
+    actual = main(start, end)
+
+    output_response = requests.get(f'https://tsserv.tinkermode.dev/hourly?begin={convert_datetime_to_string(start)}&end={convert_datetime_to_string(end)}')
+    output = digest_request(output_response.text)
+    assert actual == output
 #
-#     actual = main(start, end)
-#     output_response = requests.get(f'https://tsserv.tinkermode.dev/hourly?begin={convert_datetime_to_string(start)}&end={convert_datetime_to_string(end)}')
-#     output = digest_request(output_response.text)
-#     assert actual == output
-#
-# def test_two():
-#     start = datetime(2021, 3, 2, 4)
-#     end = datetime(2021, 3, 8, 11, 59, 59)
-#     actual = main(start, end)
-#     with open('actual.txt', 'w') as file:
-#         for time, average in actual:
-#             file.write(f'{time} {float(average)}\n')
-#
-#
-#     output_response = requests.get(f'https://tsserv.tinkermode.dev/hourly?begin={convert_datetime_to_string(start)}&end={convert_datetime_to_string(end)}')
-#     output = digest_request(output_response.text)
-#     with open('expected.txt', 'w') as file:
-#         for time, average in output:
-#             file.write(f'{time} {average}\n')
-#     print(len(actual))
-#     print(len(output))
-#     print(actual)
-#     print(output)
-#     assert actual == output
-#
-# def test_three():
-#     start = datetime(2021, 3, 2, 4)
-#     end = datetime(2021, 3, 3, 11, 59, 59)
-#
-#     actual = main(start, end)
-#     with open('actual.txt', 'w') as file:
-#         for time, average in actual:
-#             file.write(f'{time} {float(average)}\n')
-#
-#
-#     output_response = requests.get(f'https://tsserv.tinkermode.dev/hourly?begin={convert_datetime_to_string(start)}&end={convert_datetime_to_string(end)}')
-#     output = digest_request(output_response.text)
-#     with open('expected.txt', 'w') as file:
-#         for time, average in output:
-#             file.write(f'{time} {average}\n')
-#     print(len(actual))
-#     print(len(output))
-#     print(actual)
-#     print(output)
-#     assert actual == output
+def test_three():
+    start = datetime(2021, 3, 2, 4)
+    end = datetime(2021, 3, 3, 11, 59, 59)
+
+    actual = main(start, end)
+    output_response = requests.get(f'https://tsserv.tinkermode.dev/hourly?begin={convert_datetime_to_string(start)}&end={convert_datetime_to_string(end)}')
+    output = digest_request(output_response.text)
+    assert actual == output
 
 def test_four():
     start = datetime(2021, 3, 2, 4)
     end = datetime(2021, 3, 10) 
 
     actual = main(start, end)
-    with open('actual.txt', 'w') as file:
-        for time, average in actual:
-            file.write(f'{time} {float(average)}\n')
-
-
     output_response = requests.get(f'https://tsserv.tinkermode.dev/hourly?begin={convert_datetime_to_string(start)}&end={convert_datetime_to_string(end)}')
     output = digest_request(output_response.text)
-    with open('expected.txt', 'w') as file:
-        for time, average in output:
-            file.write(f'{time} {average}\n')
-    print(len(actual))
-    print(len(output))
-    print(actual)
-    print(output)
     assert actual == output
 
+# commented since it will take some time
 # def test_long():
 #     start = datetime(2021, 3, 2, 4)
 #     end = datetime(2021, 12, 3, 11, 59, 59)
 #
 #     actual = main(start, end)
-#     with open('actual.txt', 'w') as file:
-#         for time, average in actual:
-#             file.write(f'{time} {float(average)}\n')
-#
-#
 #     output_response = requests.get(f'https://tsserv.tinkermode.dev/hourly?begin={convert_datetime_to_string(start)}&end={convert_datetime_to_string(end)}')
 #     output = digest_request(output_response.text)
-#     with open('expected.txt', 'w') as file:
-#         for time, average in output:
-#             file.write(f'{time} {average}\n')
-#     print(len(actual))
-#     print(len(output))
-#     print(actual)
-#     print(output)
 #     assert actual == output
